8972b6a453d7fffb059771b3dda0d3498eaccd71
apoorvmathur <apoorvmathur@live.in>
2017-06-18 01:17:53 +0530

    changed key representation to base64

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Base64;
 import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
 
@@ -7,7 +8,7 @@ public class Test {
 	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
 		
 		KeyPair kp = Encryption.generateKey();
-		String key = new String(kp.getPublic().getEncoded());
+		String key = Base64.getEncoder().encodeToString(kp.getPublic().getEncoded());
 		
 		PubKey pubKey = new PubKey(key, "Dummy Address", "Mogambo");
 		Beacon.sendBeacon(pubKey);
