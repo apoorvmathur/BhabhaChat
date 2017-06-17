b34f6054c524c048e54f40cbc7229a6d71f890e4
m-joshi <joshi.muskan1411@gmail.com>
2017-06-17 22:42:38 +0530

    Update PubKey.java

--- src/PubKey.java
+++ src/PubKey.java
@@ -4,7 +4,7 @@ public class PubKey implements Serializable{
 	
 	private static final long serialVersionUID = 2243398784413631870L;
 	
-	private String pub_key;
+	private String pubKey;
 	private String ipAddress;
 		
 	public PubKey(String pubKey, String ipAddress) {
